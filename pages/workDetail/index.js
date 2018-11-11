892602e64760492ac0635b055b97dc9aa719f3ac
Jackie Wu <jackie.me@qq.com>
2018-11-11 23:19:29 +0800

    test

---
+++ pages/workDetail/index.js
@@ -0,0 +1,18 @@
+const fetch = require('../../utils/fetch');
+
+Page({
+  data: {
+
+
+  },
+  onLoad: function () {
+    try {
+
+    }catch (e) {
+      // $Toast({
+      //   content: '获取系统信息失败',
+      //   type: 'error'
+      // });
+    }
+  }
+});
