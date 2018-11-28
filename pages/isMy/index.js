29e266f49628346f2ad6cf3d4857090ff92a7080
Jackie Wu <jackie.me@qq.com>
2018-11-28 22:26:15 +0800

    test

--- pages/isMy/index.js
+++ pages/isMy/index.js
@@ -2,7 +2,68 @@
 
 Page({
   data: {
-
+    infoTag: [
+      {
+        name: '关注',
+        key: 'focus',
+        number: '0',
+      },
+      {
+        name: '粉丝',
+        key: 'focus',
+        number: '10',
+      },
+      {
+        name: '获赞',
+        key: 'zan',
+        number: '100',
+      },
+    ],
+    infoList: [
+      {
+        name: '我的作品',
+        icon: 'icon-zuopin1',
+        key: 'product',
+      },
+      {
+        name: '我的专辑',
+        icon: 'icon-zhuanjishenhe',
+        key: 'album',
+      },
+      {
+        name: '我的收藏',
+        icon: 'icon-shoucangxing2',
+        key: 'collect',
+      },
+      {
+        name: '我的关注',
+        icon: 'icon-chakantieziguanzhu',
+        key: 'focus',
+      },
+      {
+        name: '我的字库',
+        icon: 'icon-kucunguanli',
+        key: 'warehouse',
+      },
+      {
+        name: '我的图章',
+        icon: 'icon-fl-zhang',
+        key: 'stamp',
+      },
+      {
+        name: '已度商城',
+        icon: 'icon-shangcheng',
+        key: 'store',
+      },
+      {
+        name: '我的订单',
+        icon: 'icon-tianchongxing-',
+        key: 'indent',
+      },
+    ],
+    lvNumber: 10,
+    desc: '留下一句话留下一句话留下一句话留下一句话留下一句话留下一句话',
+    photo: 'http://demo.sc.chinaz.com//Files/DownLoad/webjs1/201503/jiaoben3190/images/rustic-landscape.jpg'
   },
 
 });
