c5dfe05cc0e6acb7c9aff99deafc7113895c707b
Jackie Wu <jackie.me@qq.com>
2018-12-06 21:50:40 +0800

    test

--- pages/enjoy/index.js
+++ pages/enjoy/index.js
@@ -1,34 +1,34 @@
 Page({
   data: {
+    tabs: ['赏析', '照片', '音频', '视频'],
+    character: [
+      {
+        value: '学长他就像我生命中的灵感，他让我了解爱的积极意义，他就像是让我一直前进的动力， 其实这部在内地12年上映的泰国校园爱情小清新电影，每次想到学生时期的那份纯纯爱恋，总是会把它再刷一遍，遍遍的感触都不同，就像那个她永远在我心裡，但却越来越模糊。 校园+淡淡初恋+死党...'
+      }
+    ],
+
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
+
+
     indicatorDots: false,
     autoplay: false,
-    interval: 5000,
     duration: 200,
     circular: false
   },
-  changeIndicatorDots: function(e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
+  tabsChange:(e) => {
+    console.log()
   },
-  changeAutoplay: function(e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
+  swiperAnimatEnd: function (e) {
+    const { imgUrls } = this.data;
+    if (e.detail.current > (this.data.imgUrls.length - 2)) {
+      imgUrls.push('http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg', 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg');
+      this.setData({
+        imgUrls
+      });
+
+    }
   },
-  intervalChange: function(e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange: function(e) {
-    this.setData({
-      duration: e.detail.value
-    })
-  }
 })
