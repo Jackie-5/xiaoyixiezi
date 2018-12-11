eb01a9ac9230e575634395e2dc7594f4a2751a34
Jackie Wu <jackie.me@qq.com>
2018-12-11 22:42:07 +0800

    test

--- pages/workDetail/index.js
+++ pages/workDetail/index.js
@@ -1,66 +1,91 @@
-import { $wuxGallery } from '../../dist/index'
 import fetch from '../../utils/fetch';
 import requestConfig from "../../utils/config/request";
+import { $wuxActionSheet } from '../../dist/index';
 
-const { getInfoList, getAlbumList } = requestConfig.workDetail;
+const { getInfoList } = requestConfig.workDetail;
 
 Page({
   data: {
-    detailList: [
-      {
-        image: 'http://demo.sc.chinaz.com//Files/DownLoad/webjs1/201503/jiaoben3190/images/rustic-landscape.jpg',
-        key: 'loading'
-      }
-    ],
+    detailList: [],
     currentPage: 0,
+    pageSize: 10,
     totalPage: 0,
+    duration: 200,
+    getImageNumber: 6,
+    current: 0,
+    currentData: {},
+    swiperIndex: 0,
   },
   onPullDownRefresh () {
     wx.stopPullDownRefresh()
   },
   onLoad() {
     this.getImgInfo();
-
-    this.$wuxGallery = $wuxGallery();
-    this.$wuxGallery.show();
   },
-
-  getImgInfo() {
-    const self = this;
-    fetch(getInfoList, {
-      currentPage: this.data.currentPage,
-    }).then((data) => {
-      console.log('12313');
-      let detailList = [];
-      this.data.detailList.forEach((item) => {
-        if (item.key !== 'loading') {
-          detailList.push(item);
-        }
-      });
-
-      detailList = [
-        ...detailList,
-        ...data.data.data,
-      ];
-
-
-      if (data.data.currentPage < data.data.totalPage) {
-        detailList.push({
-          image: 'http://img.lanrentuku.com/img/allimg/1212/5-121204193R0-50.gif',
-          key: 'loading',
-        });
-      }
-
-      this.setData({
-        detailList,
-        currentPage: data.data.currentPage - 1,
-        totalPage: data.data.totalPage,
-      }, function () {
-        this.$wuxGallery.setImage({
-          urls: detailList,
-          current: this.data.currentPage
-        })
-      });
+  onTap(e) {
+    const item = e.target.dataset.item;
+    wx.previewImage({
+      current: item.image,
+      urls: [ item.image ],
     });
   },
-})
+
+  animationFinish(e) {
+    const current = e.detail.current + 1;
+    const { getImageNumber, swiperIndex, totalPage } = this.data;
+    if ((current % getImageNumber) === 0 && current > swiperIndex && current < (totalPage / 2)) {
+      this.getImgInfo();
+      this.setData({
+        swiperIndex: current,
+      })
+    }
+    this.setData({
+      current: e.detail.current,
+      currentData: this.data.detailList[e.detail.current],
+    });
+  },
+
+
+  getImgInfo() {
+    fetch(getInfoList, {
+      currentPage: this.data.currentPage,
+      pageSize: this.data.pageSize,
+    }).then((data) => {
+      const dataObj = {
+        detailList: data.data,
+        totalPage: data.totalPage,
+      };
+      if (this.data.detailList && this.data.detailList.length > 0) {
+        dataObj.detailList = [...this.data.detailList, ...data.data];
+      } else {
+        dataObj.currentData = dataObj.detailList[0];
+      }
+      this.setData(dataObj)
+    });
+  },
+
+  onPraise() {
+    const { currentData } = this.data;
+    console.log(currentData);
+  },
+
+  onComments() {
+    const { currentData } = this.data;
+    console.log('123123');
+    $wuxActionSheet().showSheet({
+      theme: 'wx',
+      titleText: '三秒后自动关闭',
+      buttons: [{
+        text: '实例菜单'
+      },
+        {
+          text: '实例菜单'
+        },
+      ],
+      buttonClicked(index, item) {
+        return true
+      },
+    })
+  }
+
+});
